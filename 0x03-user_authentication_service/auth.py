7a0a2eada18271180b06eba1963cb0e8aa585137
Georgen21 <chukwudinwabia42@gmail.com>
2024-06-12 04:02:42 +0000

    Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-"""
-An authentication module
-"""
+""" Authentication module """
 import bcrypt
+
 
 def _hash_password(password: str) -> bytes:
     """
     Hashes a password string and returns it in bytes form
     Args:
         password (str): password in string format
-    Returns:
-        bytes
     """
     passwd = password.encode('utf-8')
     return bcrypt.hashpw(passwd, bcrypt.gensalt())
