0ae6417aa69b9718b14d9508fca6d6bb18bf28f0
Georgen21 <chukwudinwabia42@gmail.com>
2024-06-13 11:36:22 +0000

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 A simple flask app
 """
-from flask import Flask, request, jsonify, abort, redirect
+from flask import Flask, request, jsonify, abort, redirect, url_for
 from auth import Auth
 
 
@@ -77,6 +77,20 @@ def profile() -> str:
     if user:
         return jsonify({"email": f"{user.email}"}), 200
     abort(403)
+
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Generate a token for resetting a user's password
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"})
 
 
 if __name__ == "__main__":
